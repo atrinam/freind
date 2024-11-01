7b1af757a2a59bd8ead4e40d10835e13c43e8160
moharanger <m33537924@gmail.com>
2024-11-01 19:23:32 +0330

    Update __init__.py

--- YukkiMusic/platforms/__init__.py
+++ YukkiMusic/platforms/__init__.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from .Apple import AppleAPI
 from .Carbon import CarbonAPI
 from .Resso import RessoAPI
