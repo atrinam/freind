1742f4f6e6819fcd73b35f051e41a94d101295e7
atrinam <moha.face9@gmail.com>
2024-12-13 09:47:47 +0330

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -1,30 +1,19 @@
-
-import uvloop
-
-uvloop.install()
-
-
 import sys
 
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.types import (
-    BotCommand,
-    BotCommandScopeAllChatAdministrators,
-    BotCommandScopeAllGroupChats,
-    BotCommandScopeAllPrivateChats,
-)
+from pyrogram.types import BotCommand
 
 import config
 
 from ..logging import LOGGER
 
 
-class YukkiBot(Client):
+class KNBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"در حال راه اندازی ربات")
         super().__init__(
-            "PyKalier",
+            "KNMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -42,62 +31,55 @@ class YukkiBot(Client):
         try:
             await self.send_message(
                 config.LOG_GROUP_ID,
-                text=f"<u><b>{self.mention} ربات شروع به کار کرد :</b><u>\n\nآیدی  : <code>{self.id}</code>\nنام : {self.name}\nنام کاربری  : @{self.username}",
+                text=f"<blockquote><b>{self.mention} ربات فعال شد :</b><u>\n\nشناسه : <code>{self.id}</code>\nنام : {self.name}\nنام کاربری : @{self.username} </b></blockquote>",
             )
         except:
             LOGGER(__name__).error(
-                "ربات نتوانسته به گروه لاگ دسترسی پیدا کند. مطمئن شوید که ربات خود را به کانال لاگ اضافه کرده‌اید و به عنوان مدیر ارتقا داده‌اید!"
+                "ربات نمیتواند به گروه لاگ دسترسی پیدا کند. لطفا ربات را به کانال لاگ اضافه کرده و ادمین کنید!"
             )
-            # sys.exit()
-        if config.SET_CMDS == str(True):
+            sys.exit()
+        if config.SET_CMDS:
             try:
-
                 await self.set_bot_commands(
-                    commands=[
-                        BotCommand("start", "شروع ربات"),
-                        BotCommand("help", "دستورات کمک"),
-                        BotCommand("ping", "چک کردن ربات"),
-                    ],
-                    scope=BotCommandScopeAllPrivateChats(),
-                )
-                await self.set_bot_commands(
-                    commands=[
-                        BotCommand("play", "شروع پخش موزیک"),
-                    ],
-                    scope=BotCommandScopeAllGroupChats(),
-                )
-                await self.set_bot_commands(
-                    commands=[
-                        BotCommand("play", "شروع پخش موزیک"),
-                        BotCommand("skip", "رفتن به موزیک بعدی"),
-                        BotCommand("pause", "توقف موزیک در حال پخش"),
-                        BotCommand("resume", "ادامه موزیک"),
-                        BotCommand("end", "بستن پخش ویس چت"),
-                        BotCommand("shuffle", "پخش اتفاقی"),
-                        BotCommand(
-                            "playmode",
-                            "دیدن حالت پخش",
-                        ),
-                        BotCommand(
-                            "settings",
-                            "باز کردن تنظیمات ربات برای چت شما.",
-                        ),
-                    ],
-                    scope=BotCommandScopeAllChatAdministrators(),
+                    [
+                        BotCommand("start", "🤖 شروع کار با ربات"),
+                        BotCommand("help", "📚 راهنمای دستورات"),
+                        BotCommand("ping", "📡 بررسی وضعیت ربات"),
+                        BotCommand("play", "▶️ پخش موزیک"),
+                        BotCommand("vplay", "🎥 پخش ویدیو"),
+                        BotCommand("playlist", "📋 لیست پخش"),
+                        BotCommand("skip", "⏭️ رد کردن"),
+                        BotCommand("pause", "⏸️ توقف موقت"),
+                        BotCommand("resume", "▶️ ادامه پخش"),
+                        BotCommand("stop", "⏹️ توقف پخش"),
+                        BotCommand("shuffle", "🔀 پخش تصادفی"),
+                        BotCommand("seek", "⏩ جلو/عقب بردن"),
+                        BotCommand("reboot", "🔄 راه اندازی مجدد"),
+                        BotCommand("settings", "⚙️ تنظیمات"),
+                        BotCommand("stats", "📊 آمار ربات"),
+                        BotCommand("sudolist", "👥 لیست سودو"),
+                        BotCommand("lyrics", "📝 متن آهنگ"),
+                        BotCommand("queue", "📝 صف پخش"),
+                        BotCommand("clean", "🧹 پاکسازی"),
+                        BotCommand("auth", "👤 کاربر مجاز"),
+                        BotCommand("unauth", "🚫 لغو کاربر مجاز"),
+                        BotCommand("block", "🚫 مسدود کردن"),
+                        BotCommand("unblock", "✅ رفع مسدودی"),
+                        BotCommand("blacklist", "⚠️ لیست سیاه"),
+                        BotCommand("whois", "👤 اطلاعات کاربر"),
+                        BotCommand("gbans", "🚫 لیست گلوبال بن"),
+                    ]
                 )
             except:
                 pass
         else:
             pass
-        try:
-            a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-            if a.status != ChatMemberStatus.ADMINISTRATOR:
-                LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
-                sys.exit()
-        except Exception:
-            pass
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(__name__).error("لطفاً ربات را در گروه لاگ ادمین کنید")
+            sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"ربات موزیک {self.name} با موفقیت راه اندازی شد")
